d830a394ab00e49236507ccb0aab21ea30d08751
ThaGary <garyanderson31591@gmail.com>
2018-11-27 13:42:13 -0700

    set up queries

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
+const queries = require('./queries')
+const config = require('./knexfile')
 
 app.listen(port)
 
 app.get('/', (req, res) => {
-    res.send('THE ROUTE WORKED!')
+    queries.getAll().then(response => res.send(response))
 })
