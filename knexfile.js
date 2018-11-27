8d2332d04c2aede20d5d593e9db0d119fd0f0b8d
ThaGary <garyanderson31591@gmail.com>
2018-11-27 09:57:11 -0700

    knex setup

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/DATABASE_NAME_THAT_MAKES_SENSE_FOR_G102_DATABASE'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  }
+
+};
