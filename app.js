b935b2cb90102677716bc43210c3cca0694f8e98
ThaGary <garyanderson31591@gmail.com>
2018-11-28 10:11:10 -0700

    getbyid function working

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const app = express()
 const port = process.env.PORT || 3001
 const queries = require('./queries')
 const config = require('./knexfile')
+const bodyParser = require('body-parser')
+var cors = require('cors')
+ 
+app.use(cors())
+
+app.use(bodyParser.json())
 
 app.listen(port)
 
 app.get('/', (req, res) => {
-    queries.getAll().then(response => res.send(response))
+    queries.getAll().then(res => res.send(res))
+})
+
+app.get('/:id', (req, res) => {
+    queries.getById(req.params.id).then(students => res.send(students))
 })
--- queries.js
+++ queries.js
@@ -3,5 +3,8 @@ const db = require('./database-connection')
 module.exports = {
     getAll() {
         return db('students')
+    },
+    getById(id){
+        return db('students').where({id: id}).first()
     }
 }
